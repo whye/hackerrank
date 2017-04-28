ead2c2e9254eea9da16cba30aa29d69b9a447fa7
Hanye Wei <andy.hanye.wei@gmail.com>
2017-04-28 15:14:05 -0500

    Create Day 0: Weighted Mean.cpp
    https://www.hackerrank.com/challenges/s10-weighted-mean

---
+++ Day 0: Weighted Mean.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin >> n;
+    
+    vector<int> num(n);
+    vector<int> weight(n);
+    int sum = 0;
+    int sumweight = 0;
+    for(int i = 0; i < n; i++){
+        cin >> num[i];
+    }
+    for(int i = 0; i < n; i++){
+        cin >> weight[i];
+        sum += num[i] * weight[i];
+        sumweight += weight[i];
+    }
+    
+    float ans = (float)sum/((float)sumweight*1.0);
+    
+    cout.setf(ios::fixed);
+    cout << setprecision(1) << ans  << endl;
+    
+    return 0;
+}
