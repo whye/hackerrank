51ed1e1674ddee3ef3385cf0430493355a6215e8
Hanye Wei <andy.hanye.wei@gmail.com>
2017-05-01 21:58:03 -0500

    Create Day 7: Arrays.cpp

---
+++ 30 Days of Code Challenges/Day 7: Arrays.cpp
@@ -0,0 +1,40 @@
+//https://www.hackerrank.com/challenges/30-arrays
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0;arr_i < n;arr_i++){
+       cin >> arr[arr_i];
+    }
+    
+    for(int rev = n-1; rev >= 0; rev--){
+        cout << arr[rev] << " ";
+    }
+    return 0;
+}
