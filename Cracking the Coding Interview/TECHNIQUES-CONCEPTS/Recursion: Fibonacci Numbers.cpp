1ccf5a50157b543ac8ab1419f341fc490e68de64
Hanye Wei <andy.hanye.wei@gmail.com>
2017-05-03 23:10:38 -0500

    Create Recursion: Fibonacci Numbers.cpp

---
+++ Cracking the Coding Interview/TECHNIQUES-CONCEPTS/Recursion: Fibonacci Numbers.cpp
@@ -0,0 +1,18 @@
+//https://www.hackerrank.com/challenges/ctci-fibonacci-numbers
+
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int n) {
+    // Complete the function.
+    if(n == 0 || n == 1) return n;
+    else return fibonacci(n - 1) + fibonacci(n - 2);
+
+}
+int main() {
+    int n;
+    cin >> n;
+    cout << fibonacci(n);
+    return 0;
+}
