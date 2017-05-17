064a4f100ded6b7ccda1676cad7836ced6dbd8c8
Hanye Wei <andy.hanye.wei@gmail.com>
2017-05-17 15:53:19 -0500

    Create Day 22: Binary Search Trees.cpp

---
+++ 30 Days of Code Challenges/Day 22: Binary Search Trees.cpp
@@ -0,0 +1,69 @@
+//https://www.hackerrank.com/challenges/30-binary-search-trees
+
+#include <iostream>
+#include <cstddef>
+
+using namespace std;	
+
+class Node{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+class Solution{
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            }
+            else {
+                Node* cur;
+                if(data <= root->data){
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                }
+                else{
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+        		int getHeight(Node* root){
+          //Write your code here
+          if(root == NULL) return 0;
+          else if(root->left == NULL && root->right == NULL) return 0;
+          else return max(getHeight(root->left),getHeight(root->right)) + 1;
+        }
+
+
+}; //End of Solution
+
+int main() {
+    Solution myTree;
+    Node* root = NULL;
+    int t;
+    int data;
+
+    cin >> t;
+
+    while(t-- > 0){
+        cin >> data;
+        root = myTree.insert(root, data);
+    }
+    int height = myTree.getHeight(root);
+    cout << height;
+
+    return 0;
+}
+    
+       
+            
+        
