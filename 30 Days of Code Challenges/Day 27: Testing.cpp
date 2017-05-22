337bedeede7f9ae23923facb57bf7ef881e5ede2
Hanye Wei <andy.hanye.wei@gmail.com>
2017-05-22 08:06:54 -0500

    Create Day 27: Testing.cpp

---
+++ 30 Days of Code Challenges/Day 27: Testing.cpp
@@ -0,0 +1,12 @@
+//https://www.hackerrank.com/challenges/30-testing
+// this is a copy from others... 
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"5"<<endl; 
+    cout<<"5 3"<<endl; cout<<"-1 90 999 100 0"<<endl;
+    cout<<"4 2"<<endl; cout<<"0 -1 2 1"<<endl;
+    cout<<"3 3"<<endl; cout<<"-1 0 1"<<endl;
+    cout<<"6 1"<<endl; cout<<"-1 0 1 -1 2 3"<<endl;
+    cout<<"7 3"<<endl; cout<<"-1 0 1 2 3 4 5"<<endl;   
+}
