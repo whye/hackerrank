434b1cafbd71ae9ef795d8d8183f1aef0a8a7ddf
Hanye Wei <andy.hanye.wei@gmail.com>
2017-05-25 01:40:58 -0500

    Create Matching Specific String.py

---
+++ Regex/Introduction/Matching Specific String.py
@@ -0,0 +1,14 @@
+//https://www.hackerrank.com/challenges/matching-specific-string
+
+Regex_Pattern = r'hackerrank'	# Do not delete 'r'.
+
+
+
+
+import re
+
+Test_String = input()
+
+match = re.findall(Regex_Pattern, Test_String)
+
+print("Number of matches :", len(match))
